865aed8c0310163b882a494040706bf6fc470e18
Govind Parashurama <govind.parashurama@journey.ai>
2020-03-17 15:47:34 +0530

    Modified UI with stat, stop and added textview.

--- Andriod/app/src/main/java/com/harman/hmmediasdkapp/MainActivity.java
+++ Andriod/app/src/main/java/com/harman/hmmediasdkapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.harman.hmmediasdkapp;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.annotation.Keep;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonStart = null;
+    private Button buttonStop = null;
+
+    private TextView displayView = null;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,15 +24,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-        String metadata = "{filename:driver-1234-1232323.mp4, gps-latitude:28.674109, gps-longitude:77.438009, timestamp:20200309194530, uploadmode:normal}";
-
-        //socket.upload( filename, , metadata);
-
-        upload("Upload", metadata, "/storage/emulated/0/Android/data/com.harman.hmmediasdkapp/files/Harman/test.mp4");
-
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText("arvind");
+    private void initViews() {
+        displayView = findViewById(R.id.displayId);
+        buttonStart = findViewById(R.id.start);
+        buttonStart.setOnClickListener(this);
+        buttonStop = findViewById(R.id.stop);
+        buttonStop.setOnClickListener(this);
     }
 
 
@@ -33,20 +40,34 @@ public class MainActivity extends AppCompatActivity {
      * A function calling from JNI to update current timer
      */
     @Keep
-    private void updateTimer(final String msg ) {
-
-        Log.e("JniHandler1", "Native Err: " + msg);
-
-
+    private void updateTimer(final String msg) {
+        Log.d("JniHandler1", "Native Err: " + msg);
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-
-                //MainActivity.this.tickView.setText(msg + " " );
-                TextView tv = findViewById(R.id.sample_text);
-                tv.setText(msg);
+                Log.d("JniHandler1", "Native Err: $msg");
+                String s = displayView.getText().toString();
+                displayView.append(s+"\n");
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.start:
+                displayView.setText("");
+                String metadata =
+                        "{filename:driver-1234-1232323.mp4, gps-latitude:28.674109, gps-longitude:77.438009, timestamp:20200309194530, uploadmode:normal}";
+                upload(
+                        "Upload", metadata,
+                        "/storage/emulated/0/Android/data/com.harman.hmmediasdkapp/files/Harman/test.mp4");
+                break;
+            case R.id.stop:
+                stop();
+                break;
+        }
     }
 
     /**
@@ -56,4 +77,6 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
 
     public native void upload(String driverid, String metadata, String path);
+
+    public native void stop();
 }
