aa1981b0e0bca018357019a2d597d609dbfaa764
Arvind Umrao <arvind.umrao@sococo.com>
2020-03-30 11:53:24 +0530

    changed metadata

--- IOS/main.cpp
+++ IOS/main.cpp
@@ -9,7 +9,8 @@ int main(int argc, char** argv) {
     hm::init();
 
     std::string file = "./test.mp4"; //complete path
-    std::string metadata = "{filename:driver-1234-1232323.mp4, gps-latitude:28.674109, gps-longitude:77.438009, timestamp:20200309194530, uploadmode:normal}";
+    
+    std::string  metadata ="{\"filename\":\"1.mp4\",\"gps-latitude\":\"28.674109\",\"gps-longitude\":\"77.438009\",\"timestamp\":\"20200309194530\",\"uploadmode\":\"normal\"}";
 
     hm::upload("driver-1234", metadata, file);
 
